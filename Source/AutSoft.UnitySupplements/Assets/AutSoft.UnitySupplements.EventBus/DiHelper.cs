7f7acae67687b81bc055d2c120a09b40601222cb
Bacskay Viktor <bacskay.viktor@autsoft.hu>
2022-02-11 15:18:14 +0100

    add eventbus package

---
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.EventBus/DiHelper.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
+
+namespace AutSoft.UnitySupplements.EventBus
+{
+    public static class DiHelper
+    {
+        public static void AddEventBus(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            services.AddSingleton<IEventBus, SimpleEventBus>();
+            services.AddTransient<ServiceFactory>(sp => sp.GetRequiredService);
+
+            services.Scan(scan => scan
+                .FromAssemblies(assemblies)
+                .AddClasses(classes =>
+                    classes.Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEventHandler<>))))
+                .AsImplementedInterfaces()
+                .WithTransientLifetime());
+        }
+    }
+}
---
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.EventBus/IEventHandler.cs
@@ -0,0 +1,8 @@
+﻿#nullable enable
+namespace AutSoft.UnitySupplements.EventBus
+{
+    public interface IEventHandler<in T> where T : IEvent
+    {
+        void Handle(T message);
+    }
+}
