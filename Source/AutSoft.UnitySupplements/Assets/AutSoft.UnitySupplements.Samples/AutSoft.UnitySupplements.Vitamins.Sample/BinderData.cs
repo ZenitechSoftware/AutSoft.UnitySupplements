5e61fc97cc65c0e7a394840d91fcd0aa1765986c
Kurai András <kurai.andras@autsoft.hu>
2022-09-10 20:19:01 +0200

    add binding sample

---
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Samples/AutSoft.UnitySupplements.Vitamins.Sample/BinderData.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace AutSoft.UnitySupplements.Vitamins.Sample
+{
+    public class SampleData : ObservableObject
+    {
+        private string? _input;
+
+        public string? Input
+        {
+            get => _input;
+            set => SetProperty(ref _input, value);
+        }
+    }
+}
---
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Samples/AutSoft.UnitySupplements.Vitamins.Sample/BindingSample.cs
@@ -0,0 +1,41 @@
+﻿#nullable enable
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AutSoft.UnitySupplements.Vitamins.Sample
+{
+    public class BindingSample : MonoBehaviour
+    {
+        private readonly SampleData _data = new();
+
+        [SerializeField] private TMP_InputField _inputField = default!;
+        [SerializeField] private TMP_Text _inputShowcase = default!;
+        [SerializeField] private Button _removeLastCharacterButton = default!;
+
+        private void Awake()
+        {
+            this.CheckSerializedField(x => x._inputField);
+            this.CheckSerializedField(x => x._removeLastCharacterButton);
+            this.CheckSerializedField(x => x._inputShowcase);
+
+            _data.BindTwoWay
+            (
+                gameObject,
+                _inputField.onValueChanged,
+                nameof(_data.Input),
+                updateTarget: x => _inputField.text = x,
+                updateSource: x => x
+            );
+
+            _data.BindOneWay<string>(gameObject, nameof(_data.Input), i => _inputShowcase.text = i);
+
+            _removeLastCharacterButton.onClick.AddWeak(gameObject, () =>
+            {
+                if (_data.Input?.Length == 0) return;
+
+                _data.Input = _data.Input?[..^1];
+            });
+        }
+    }
+}
