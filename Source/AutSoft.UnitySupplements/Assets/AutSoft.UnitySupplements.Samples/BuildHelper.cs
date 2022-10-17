971be81658542fe6f2f838af0ec081bc6cdbaadd
Kurai András <k.andras9642@gmail.com>
2022-10-17 11:26:46 +0200

    build helper uses generated editor prefs

--- Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Samples/BuildHelper.cs
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Samples/BuildHelper.cs
@@ -1,8 +1,8 @@
 ﻿#nullable enable
+using AutSoft.UnitySupplements.Samples.ResourceGeneratorSamples;
 using System.IO;
 using System.Runtime.InteropServices;
 using Unity.CodeEditor;
-using UnityEditor;
 using UnityEngine;
 
 namespace AutSoft.UnitySupplements.Samples
@@ -13,24 +13,18 @@ namespace AutSoft.UnitySupplements.Samples
         {
             if (!RuntimeInformation.OSDescription.Contains("Windows")) return;
 
-            const string vs2019 = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\devenv.exe";
             const string vs2022 = @"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe";
-            if (File.Exists(vs2019))
+            if (File.Exists(vs2022))
             {
                 Debug.Log("Setting VS2022 as default");
-                EditorPrefs.SetString("kScriptsDefaultApp", vs2019);
-            }
-            else if (File.Exists(vs2022))
-            {
-                Debug.Log("Setting VS2022 as default");
-                EditorPrefs.SetString("kScriptsDefaultApp", vs2022);
+                ResourcePaths.KnownEditorPrefs.CodeEditor.SetkScriptsDefaultApp(vs2022);
             }
             else
             {
                 Debug.LogError("Cloud not find Visual Studio installation");
             }
 
-            EditorPrefs.SetInt("unity_project_generation_flag", 255);
+            ResourcePaths.KnownEditorPrefs.CodeEditor.Setunity_project_generation_flag(255);
 
             CodeEditor.Editor.CurrentCodeEditor.SyncAll();
         }
