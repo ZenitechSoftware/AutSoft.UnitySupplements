5282f0c090d6b75dc8c9060902170f52fd865a66
Kurai András <kurai.andras@autsoft.hu>
2022-10-21 14:55:14 +0200

    more comments

--- Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Vitamins/Bindings/CollectionBindings.cs
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Vitamins/Bindings/CollectionBindings.cs
@@ -10,27 +10,37 @@ namespace AutSoft.UnitySupplements.Vitamins.Bindings
         /// <summary>
         /// One-way Collection
         /// </summary>
+        /// <typeparam name="TBindingSource">Type of the class we are bindig to</typeparam>
+        /// <typeparam name="TItem">The collection's type</typeparam>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="source">The binding source</param>
+        /// <param name="sourceToTarget"></param>
         public static void Bind<TBindingSource, TItem>
         (
             this MonoBehaviour lifetimeOwner,
             TBindingSource source,
-            Action<CollectionChangedArgs<TItem>> update
+            Action<CollectionChangedArgs<TItem>> sourceToTarget
         )
             where TBindingSource : INotifyCollectionChanged, IEnumerable<TItem> =>
-            lifetimeOwner.gameObject.Bind(source, update);
+            lifetimeOwner.gameObject.Bind(source, sourceToTarget);
 
         /// <summary>
         /// One-way Collection
         /// </summary>
+        /// <typeparam name="TBindingSource">Type of the class we are bindig to</typeparam>
+        /// <typeparam name="TItem">The collection's type</typeparam>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="source">The binding source</param>
+        /// <param name="sourceToTarget"></param>
         public static void Bind<TBindingSource, TItem>
         (
             this GameObject lifetimeOwner,
             TBindingSource source,
-            Action<CollectionChangedArgs<TItem>> update
+            Action<CollectionChangedArgs<TItem>> sourceToTarget
         )
             where TBindingSource : INotifyCollectionChanged, IEnumerable<TItem>
         {
-            void CollectionChanged(object _, NotifyCollectionChangedEventArgs e) => update(new CollectionChangedArgs<TItem>(e));
+            void CollectionChanged(object _, NotifyCollectionChangedEventArgs e) => sourceToTarget(new CollectionChangedArgs<TItem>(e));
 
             source.CollectionChanged += CollectionChanged;
             lifetimeOwner.GetOrAddComponent<DestroyDetector>().Destroyed += () => source.CollectionChanged -= CollectionChanged;
--- Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Vitamins/Bindings/UnityEventBindings.cs
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Vitamins/Bindings/UnityEventBindings.cs
@@ -5,52 +5,112 @@ namespace AutSoft.UnitySupplements.Vitamins.Bindings
 {
     public static partial class Binder
     {
-        public static void Bind(this MonoBehaviour owner, UnityEvent unityEvent, UnityAction action) =>
-            owner.gameObject.Bind(unityEvent, action);
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind(this MonoBehaviour lifetimeOwner, UnityEvent unityEvent, UnityAction action) =>
+            lifetimeOwner.gameObject.Bind(unityEvent, action);
 
-        public static void Bind(this GameObject owner, UnityEvent unityEvent, UnityAction action)
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind(this GameObject lifetimeOwner, UnityEvent unityEvent, UnityAction action)
         {
-            var destroyDetector = owner.GetOrAddComponent<DestroyDetector>();
+            var destroyDetector = lifetimeOwner.GetOrAddComponent<DestroyDetector>();
             unityEvent.AddListener(action);
             destroyDetector.Destroyed += () => unityEvent.RemoveListener(action);
         }
 
-        public static void Bind<T>(this MonoBehaviour owner, UnityEvent<T> unityEvent, UnityAction<T> action) =>
-            owner.gameObject.Bind(unityEvent, action);
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind<T>(this MonoBehaviour lifetimeOwner, UnityEvent<T> unityEvent, UnityAction<T> action) =>
+            lifetimeOwner.gameObject.Bind(unityEvent, action);
 
-        public static void Bind<T>(this GameObject owner, UnityEvent<T> unityEvent, UnityAction<T> action)
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind<T>(this GameObject lifetimeOwner, UnityEvent<T> unityEvent, UnityAction<T> action)
         {
-            var destroyDetector = owner.GetOrAddComponent<DestroyDetector>();
+            var destroyDetector = lifetimeOwner.GetOrAddComponent<DestroyDetector>();
             unityEvent.AddListener(action);
             destroyDetector.Destroyed += () => unityEvent.RemoveListener(action);
         }
 
-        public static void Bind<T0, T1>(this MonoBehaviour owner, UnityEvent<T0, T1> unityEvent, UnityAction<T0, T1> action) =>
-            owner.gameObject.Bind(unityEvent, action);
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind<T0, T1>(this MonoBehaviour lifetimeOwner, UnityEvent<T0, T1> unityEvent, UnityAction<T0, T1> action) =>
+            lifetimeOwner.gameObject.Bind(unityEvent, action);
 
-        public static void Bind<T0, T1>(this GameObject owner, UnityEvent<T0, T1> unityEvent, UnityAction<T0, T1> action)
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind<T0, T1>(this GameObject lifetimeOwner, UnityEvent<T0, T1> unityEvent, UnityAction<T0, T1> action)
         {
-            var destroyDetector = owner.GetOrAddComponent<DestroyDetector>();
+            var destroyDetector = lifetimeOwner.GetOrAddComponent<DestroyDetector>();
             unityEvent.AddListener(action);
             destroyDetector.Destroyed += () => unityEvent.RemoveListener(action);
         }
 
-        public static void Bind<T0, T1, T2>(this MonoBehaviour owner, UnityEvent<T0, T1, T2> unityEvent, UnityAction<T0, T1, T2> action) =>
-            owner.gameObject.Bind(unityEvent, action);
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind<T0, T1, T2>(this MonoBehaviour lifetimeOwner, UnityEvent<T0, T1, T2> unityEvent, UnityAction<T0, T1, T2> action) =>
+            lifetimeOwner.gameObject.Bind(unityEvent, action);
 
-        public static void Bind<T0, T1, T2>(this GameObject owner, UnityEvent<T0, T1, T2> unityEvent, UnityAction<T0, T1, T2> action)
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind<T0, T1, T2>(this GameObject lifetimeOwner, UnityEvent<T0, T1, T2> unityEvent, UnityAction<T0, T1, T2> action)
         {
-            var destroyDetector = owner.GetOrAddComponent<DestroyDetector>();
+            var destroyDetector = lifetimeOwner.GetOrAddComponent<DestroyDetector>();
             unityEvent.AddListener(action);
             destroyDetector.Destroyed += () => unityEvent.RemoveListener(action);
         }
 
-        public static void Bind<T0, T1, T2, T3>(this MonoBehaviour owner, UnityEvent<T0, T1, T2, T3> unityEvent, UnityAction<T0, T1, T2, T3> action) =>
-            owner.gameObject.Bind(unityEvent, action);
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind<T0, T1, T2, T3>(this MonoBehaviour lifetimeOwner, UnityEvent<T0, T1, T2, T3> unityEvent, UnityAction<T0, T1, T2, T3> action) =>
+            lifetimeOwner.gameObject.Bind(unityEvent, action);
 
-        public static void Bind<T0, T1, T2, T3>(this GameObject owner, UnityEvent<T0, T1, T2, T3> unityEvent, UnityAction<T0, T1, T2, T3> action)
+        /// <summary>
+        /// Subscribe to a <see cref="UnityEvent"/> for the lifetime of the <see cref="GameObject"/>
+        /// </summary>
+        /// <param name="lifetimeOwner">The binding is bound to the <see cref="GameObject"/>'s destruction</param>
+        /// <param name="unityEvent">The event we subscribe to</param>
+        /// <param name="action"><see cref="UnityAction"/> to run</param>
+        public static void Bind<T0, T1, T2, T3>(this GameObject lifetimeOwner, UnityEvent<T0, T1, T2, T3> unityEvent, UnityAction<T0, T1, T2, T3> action)
         {
-            var destroyDetector = owner.GetOrAddComponent<DestroyDetector>();
+            var destroyDetector = lifetimeOwner.GetOrAddComponent<DestroyDetector>();
             unityEvent.AddListener(action);
             destroyDetector.Destroyed += () => unityEvent.RemoveListener(action);
         }
