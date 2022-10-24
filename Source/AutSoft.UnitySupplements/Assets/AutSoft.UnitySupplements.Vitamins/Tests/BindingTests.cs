43798649c65f9e59f437dfb7547552ee3bc848d8
Kurai András <k.andras9642@gmail.com>
2022-10-24 10:02:03 +0200

    add vitamins test project

---
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Vitamins/Tests/BindingTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+
+namespace AutSoft.UnitySupplements.Vitamins.Tests
+{
+    public class BindingTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void BindingTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator BindingTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
