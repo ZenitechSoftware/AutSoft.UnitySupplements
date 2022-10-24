ca8255533c055f41298ef26343a61f25fb2c8087
Kurai András <k.andras9642@gmail.com>
2022-10-24 12:43:57 +0200

    two-way binding tests

--- Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Vitamins/Tests/BindingTests.cs
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Vitamins/Tests/BindingTests.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using NUnit.Framework;
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace AutSoft.UnitySupplements.Vitamins.Tests
 {
@@ -16,10 +17,10 @@ namespace AutSoft.UnitySupplements.Vitamins.Tests
             var vm = new BindingViewModel() { Message = "1" };
 
             // Act
-            component.Setup(vm);
+            component.BindOneWay(vm);
 
             // Assert
-            Assert.AreEqual("1", vm.Message);
+            Assert.AreEqual("1", component.Message);
         }
 
         [Test]
@@ -30,11 +31,11 @@ namespace AutSoft.UnitySupplements.Vitamins.Tests
             var vm = new BindingViewModel() { Message = "1" };
 
             // Act
-            component.Setup(vm);
+            component.BindOneWay(vm);
             vm.Message = "2";
 
             // Assert
-            Assert.AreEqual("2", vm.Message);
+            Assert.AreEqual("2", component.Message);
         }
 
         [Test]
@@ -45,13 +46,93 @@ namespace AutSoft.UnitySupplements.Vitamins.Tests
             var vm = new BindingViewModel() { Message = "1" };
 
             // Act
-            component.Setup(vm);
+            component.BindOneWay(vm);
             vm.Message = "2";
 
             UnityEngine.Object.DestroyImmediate(component.gameObject, true);
 
             // Assert
             Assert.DoesNotThrow(() => vm.Message = "3");
+        }
+
+        [Test]
+        public void Two_Way_Binding_Sets_Initial_Value()
+        {
+            // Arrange
+            var component = new GameObject().AddComponent<OneWayComponent>();
+            var vm = new BindingViewModel() { Message = "1" };
+
+            // Act
+            component.BindTwoWay(vm);
+
+            // Assert
+            Assert.AreEqual("1", component.Message);
+        }
+
+        [Test]
+        public void Two_Way_Binding_Updates_FromSource()
+        {
+            // Arrange
+            var component = new GameObject().AddComponent<OneWayComponent>();
+            var vm = new BindingViewModel() { Message = "1" };
+
+            // Act
+            component.BindTwoWay(vm);
+            vm.Message = "2";
+
+            // Assert
+            Assert.AreEqual("2", component.Message);
+        }
+
+        [Test]
+        public void Two_Way_Binding_Updates_FromTarget()
+        {
+            // Arrange
+            var component = new GameObject().AddComponent<OneWayComponent>();
+            var vm = new BindingViewModel() { Message = "1" };
+
+            // Act
+            component.BindTwoWay(vm);
+            component.ChangeMessage("2");
+
+            // Assert
+            Assert.AreEqual("2", vm.Message);
+        }
+
+        [Test]
+        public void Two_Way_Destroy_Ends_Binding()
+        {
+            // Arrange
+            var component = new GameObject().AddComponent<OneWayComponent>();
+            var vm = new BindingViewModel() { Message = "1" };
+
+            // Act
+            component.BindTwoWay(vm);
+            vm.Message = "2";
+
+            UnityEngine.Object.DestroyImmediate(component.gameObject, true);
+
+            // Assert
+            Assert.DoesNotThrow(() => vm.Message = "3");
+        }
+
+        [Test]
+        public void Binding_Lifetime_Is_Respected()
+        {
+            // Arrange
+            var component = new GameObject().AddComponent<OneWayComponent>();
+            var vm = new BindingViewModel() { Message = "1" };
+
+            // Act
+            using (var _ = component.BindTwoWay(vm))
+            {
+                component.ChangeMessage("2");
+            }
+
+            vm.Message = "3";
+
+            // Assert
+            Assert.AreEqual("2", component.Message);
         }
     }
 
@@ -70,9 +151,10 @@ namespace AutSoft.UnitySupplements.Vitamins.Tests
     {
         private bool _isDestroyed = false;
 
+        private readonly UnityEvent<string> _messageEvent = new();
         public string Message { get; set; }
 
-        public void Setup(BindingViewModel viewModel) =>
+        public BindingLifetime BindOneWay(BindingViewModel viewModel) =>
             this.Bind
             (
                 viewModel,
@@ -83,6 +165,22 @@ namespace AutSoft.UnitySupplements.Vitamins.Tests
                     Message = m;
                 }
             );
+
+        public BindingLifetime BindTwoWay(BindingViewModel viewModel) =>
+            this.Bind
+            (
+                viewModel,
+                x => x.Message,
+                m =>
+                {
+                    if (_isDestroyed) throw new InvalidOperationException();
+                    Message = m;
+                },
+                _messageEvent,
+                x => x
+            );
+
+        public void ChangeMessage(string message) => _messageEvent.Invoke(message);
 
         private void OnDestroy() => _isDestroyed = true;
     }
