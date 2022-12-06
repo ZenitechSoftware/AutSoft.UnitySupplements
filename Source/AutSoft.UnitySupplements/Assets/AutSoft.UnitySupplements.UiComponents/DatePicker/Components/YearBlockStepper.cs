07f227f5ef135aef1c52944d052053fe2f8e8e5e
Bacskay Viktor <bacskay.viktor@autsoft.hu>
2022-12-06 10:25:32 +0100

    year block stepping

---
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.UiComponents/DatePicker/Components/YearBlockStepper.cs
@@ -0,0 +1,30 @@
+﻿using AutSoft.UnitySupplements.Vitamins;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AutSoft.UnitySupplements.UiComponents.DatePicker.Components
+{
+    public class YearBlockStepper : MonoBehaviour
+    {
+        [SerializeField] private YearPicker _yearPicker = default!;
+        [SerializeField] private Button _previousButton = default!;
+        [SerializeField] private Button _nextButton = default!;
+
+        private void Awake()
+        {
+            this.CheckSerializedFields();
+            _nextButton.onClick.AddListener(StepForward);
+            _previousButton.onClick.AddListener(StepBack);
+        }
+
+        private void OnDestroy()
+        {
+            _nextButton.onClick.RemoveListener(StepForward);
+            _previousButton.onClick.RemoveListener(StepBack);
+        }
+
+        private void StepForward() => _yearPicker.StepYears(20);
+
+        private void StepBack() => _yearPicker.StepYears(-20);
+    }
+}
--- Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.UiComponents/DatePicker/Components/YearPicker.cs
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.UiComponents/DatePicker/Components/YearPicker.cs
@@ -5,15 +5,28 @@ namespace AutSoft.UnitySupplements.UiComponents.DatePicker.Components
 {
     public class YearPicker : MonoBehaviour
     {
+        [SerializeField] private Transform _buttonParent;
+
+        private int _currentYearStart;
+
+        private void Awake() => this.CheckSerializedFields();
+
         public void SpawnYears(int startYear)
         {
-            transform.DestroyChildren();
+            _buttonParent.DestroyChildren();
             startYear -= startYear % 10;
+            _currentYearStart = startYear;
             for (var i = 0; i < 20; i++)
             {
-                var currentYear = Instantiate(Resources.Load<GameObject>("YearButton"), transform);
+                var currentYear = Instantiate(Resources.Load<GameObject>("YearButton"), _buttonParent);
                 currentYear.GetComponent<YearButton>().SetupYearButton(startYear + i);
             }
+        }
+
+        public void StepYears(int step)
+        {
+            _currentYearStart += step;
+            SpawnYears(_currentYearStart);
         }
     }
 }
