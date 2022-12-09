2050e2c5a993f282fd1461a0a02f358614d6c6ef
Bacskay Viktor <bacskay.viktor@autsoft.hu>
2022-12-09 10:51:59 +0100

    toggle group uses xr toggleable

--- Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.MRTKExtras/Helpers/XrToggleGroup.cs
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.MRTKExtras/Helpers/XrToggleGroup.cs
@@ -1,9 +1,6 @@
 ﻿#nullable enable
 using AutSoft.UnitySupplements.Vitamins;
 using AutSoft.UnitySupplements.Vitamins.Bindings;
-using Injecter;
-using Microsoft.Extensions.Logging;
-using Microsoft.MixedReality.Toolkit.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +10,17 @@ namespace AutSoft.UnitySupplements.MRTKExtras.Helpers
 {
     public class XrToggleGroup : MonoBehaviour
     {
-        [Inject] private readonly ILogger<XrToggleGroup> _logger = default!;
-
         [SerializeField] private bool _allowSwitchOff = false;
 
-        [SerializeField] private List<Interactable> _toggles = default!;
+        [SerializeField] private List<XrToggleable> _toggles = default!;
 
-        private Interactable? _currentOn;
+        private XrToggleable? _currentOn;
 
         private void Awake()
         {
             foreach (var toggle in _toggles)
             {
-                if (toggle.TryGetComponent<XrToggleable>(out var xrToggleable))
-                {
-                    xrToggleable.Bind(xrToggleable.onValueChanged, OnInteractableClicked);
-                }
+                toggle.Bind(toggle.onValueChanged, OnInteractableClicked);
             }
             _currentOn = GetFirstActiveToggle();
         }
@@ -40,69 +32,49 @@ namespace AutSoft.UnitySupplements.MRTKExtras.Helpers
             set => _allowSwitchOff = value;
         }
 
-        public void AddToggleToGroup(Interactable interactable)
+        public void AddToggleToGroup(XrToggleable interactable)
         {
-            if (interactable.ButtonMode != SelectionModes.Toggle)
-            {
-                _logger.LogError("Cant add interactble to group it is not a toggle");
-                return;
-            }
-            if (interactable.TryGetComponent<XrToggleable>(out var xrToggleable))
-            {
-                xrToggleable.Bind(xrToggleable.onValueChanged, OnInteractableClicked);
-            }
-            else
-            {
-                _logger.LogError("Cant add interactble to group it is only usable with xr toggleable");
-                return;
-            }
+            interactable.Bind(interactable.onValueChanged, OnInteractableClicked);
             if (!_toggles.Contains(interactable))
+            {
                 _toggles.Add(interactable);
+            }
         }
 
         private void OnInteractableClicked(bool isOn)
         {
+            var turnedOn = _toggles.Find(t => t.IsOn && t != _currentOn);
             if (isOn)
             {
-                var turnedOn = _toggles.Find(t => t.IsToggled && t != _currentOn);
                 NotifyToggleOn(turnedOn);
                 if (!allowSwitchOff)
                 {
                     if (!_currentOn.IsObjectNull())
-                        _currentOn.IsEnabled = true;
-                    turnedOn.IsEnabled = false;
+                        _currentOn.Interactable = true;
+                    turnedOn.Interactable = false;
                 }
                 _currentOn = turnedOn;
             }
+            else if ( allowSwitchOff && turnedOn.IsObjectNull())
+            {
+                _currentOn = null;
+            }
         }
 
-        public void RemoveToggleFromGroup(Interactable interactable)
+        public void RemoveToggleFromGroup(XrToggleable interactable)
         {
-            if (interactable.ButtonMode != SelectionModes.Toggle)
-            {
-                _logger.LogError("Cant remove interactble from group it is not a toggle");
-                return;
-            }
-            if (interactable.TryGetComponent<XrToggleable>(out var xrToggleable))
-            {
-                xrToggleable.onValueChanged.RemoveListener(OnInteractableClicked);
-            }
-            else
-            {
-                _logger.LogError("Cant add interactble to group it is only usable with xr toggleable");
-                return;
-            }
+            interactable.onValueChanged.RemoveListener(OnInteractableClicked);
             if (!_toggles.Contains(interactable))
                 _toggles.Remove(interactable);
         }
 
-        private void ValidateToggleIsInGroup(Interactable toggle)
+        private void ValidateToggleIsInGroup(XrToggleable toggle)
         {
             if (toggle == null || !_toggles.Contains(toggle))
-                throw new ArgumentException(string.Format("Toggle {0} is not part of ToggleGroup {1}", new object[] { toggle, this }));
+                throw new ArgumentException($"Toggle is not part of group or null {nameof(toggle)}");
         }
 
-        public void NotifyToggleOn(Interactable toggle)
+        public void NotifyToggleOn(XrToggleable toggle)
         {
             ValidateToggleIsInGroup(toggle);
             // disable all toggles in the group
@@ -111,15 +83,15 @@ namespace AutSoft.UnitySupplements.MRTKExtras.Helpers
                 if (_toggles[i] == toggle)
                     continue;
 
-                _toggles[i].IsToggled = false;
+                _toggles[i].IsOn = false;
             }
         }
 
-        public bool AnyTogglesOn() => _toggles.Any(x => x.IsToggled);
+        public bool AnyTogglesOn() => _toggles.Any(x => x.IsOn);
 
-        public IEnumerable<Interactable> ActiveToggles() => _toggles.Where(x => x.IsToggled);
+        public IEnumerable<XrToggleable> ActiveToggles() => _toggles.Where(x => x.IsOn);
 
-        public Interactable GetFirstActiveToggle()
+        public XrToggleable GetFirstActiveToggle()
         {
             var activeToggles = ActiveToggles();
             return activeToggles.FirstOrDefault();
@@ -129,7 +101,7 @@ namespace AutSoft.UnitySupplements.MRTKExtras.Helpers
         {
             if (!allowSwitchOff && !AnyTogglesOn() && _toggles.Count != 0)
             {
-                _toggles[0].IsToggled = true;
+                _toggles[0].IsOn = true;
                 _currentOn = _toggles[0];
                 NotifyToggleOn(_toggles[0]);
             }
@@ -146,7 +118,7 @@ namespace AutSoft.UnitySupplements.MRTKExtras.Helpers
                     {
                         continue;
                     }
-                    toggle.IsToggled = false;
+                    toggle.IsOn = false;
                 }
             }
         }
