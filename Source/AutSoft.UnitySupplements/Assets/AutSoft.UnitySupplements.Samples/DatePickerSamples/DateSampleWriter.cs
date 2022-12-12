d2db068bb079a493b297557c92983ef85674ac65
Bacskay Viktor <bacskay.viktor@autsoft.hu>
2022-12-12 11:12:53 +0100

    add sample scene for datepicker testing

---
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.Samples/DatePickerSamples/DateSampleWriter.cs
@@ -0,0 +1,20 @@
+﻿using AutSoft.UnitySupplements.UiComponents.DatePicker.Components;
+using AutSoft.UnitySupplements.Vitamins;
+using AutSoft.UnitySupplements.Vitamins.Bindings;
+using TMPro;
+using UnityEngine;
+
+namespace AutSoft.UnitySupplements.Samples.DatePickerSamples
+{
+    public class DateSampleWriter : MonoBehaviour
+    {
+        [SerializeField] private DatePicker _datePicker = default!;
+        [SerializeField] private TMP_InputField _inputField = default!;
+
+        private void Awake()
+        {
+            this.CheckSerializedFields();
+            this.Bind(_datePicker.onTimePicked, date => _inputField.text = date.ToString("G"));
+        }
+    }
+}
