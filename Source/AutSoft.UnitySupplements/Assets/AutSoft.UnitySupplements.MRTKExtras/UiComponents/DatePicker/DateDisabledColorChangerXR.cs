bafa2c3d98a77b7c47dae19e3bba56b4d5c35e3a
Bacskay Viktor <bacskay.viktor@autsoft.hu>
2022-12-12 11:48:34 +0100

    day number text disabled color

---
+++ Source/AutSoft.UnitySupplements/Assets/AutSoft.UnitySupplements.MRTKExtras/UiComponents/DatePicker/DateDisabledColorChangerXR.cs
@@ -0,0 +1,26 @@
+﻿using AutSoft.UnitySupplements.UiComponents.Helpers;
+using AutSoft.UnitySupplements.Vitamins;
+using AutSoft.UnitySupplements.Vitamins.Bindings;
+using TMPro;
+using UnityEngine;
+
+namespace AutSoft.UnitySupplements.MRTKExtras.UiComponents.DatePicker
+{
+    [RequireComponent(typeof(Toggleable))]
+    public class DateDisabledColorChangerXR : MonoBehaviour
+    {
+        [SerializeField] private Color _disabledColor;
+        [SerializeField] private TMP_Text _text = default!;
+
+        private Color _originalColor;
+
+        private void Awake()
+        {
+            this.CheckSerializedFields();
+            this.Bind(GetComponent<Toggleable>().onInteractibleChanged, onStateChangedDisabledColor);
+            _originalColor = _text.color;
+        }
+
+        private void onStateChangedDisabledColor(bool isOn) => _text.color = isOn ? _originalColor : _disabledColor;
+    }
+}
